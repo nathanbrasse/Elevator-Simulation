acb630900e2572a9062462093d7ec8aef4c131df
nathanbrasse <nathanbrasseftw@gmail.com>
2025-04-10 05:36:20 -0400

    changed building

--- building.py
+++ building.py
@@ -4,26 +4,25 @@ from elevator import *
 from controller import *
 from stats import *
 from gui import *
+from collections import defaultdict
 
 class Building:
     def __init__(self, elevators, controller, stats):
         self.elevators = elevators
-        self.controller - controller
+        self.controller = controller
         self.stats = stats
 
-    def step(self, sim_time):
-        for elevator in self.elevators:
-            elevator.step(sim_time)
+        self.wait_queue = defaultdict(list)
+    
+    def get_boarding_passengers(self, floor, direction):
+        queue = self.wait_queue[floor]
+        eligible = [person for person in queue if (person.dest_floor > floor and direction=="UP")
+                    or (person.dest_floor < floor and direction == "DOWN")]
+        to_board = eligible[:MAX_CAPACITY]
+        self.wait_queue[floor] = [person for person in queue if person not in to_board]
+        return to_board
+    
+    def add_waiting_passenger(self, person):
+        self.wait_queue[person.start_floor].append(person)
 
-            #checks if passengers reach their floor
-            riders_to_exit = []
-            for person in elevator.riders:
-                if person.check_if_should_exit(elevator.current_floor, sim_time):
-                    riders_to_exit.append(person)
-                    self.stats.track_dropoff(person)
-            
-            for person in riders_to_exit:
-                elevator.riders.remove(person)
-        
-        self.controller.assign_passengers()
 
