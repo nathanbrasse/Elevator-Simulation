614708bc03130cb9caf588b875b8049c67e2b4c4
nathanbrasse <nathanbrasseftw@gmail.com>
2025-04-10 05:37:44 -0400

    changed elevator

--- elevator.py
+++ elevator.py
@@ -6,64 +6,57 @@ from stats import *
 from gui import *
 
 class Elevator:
-    def __init__(self, id, move_strat):
+    def __init__(self, id, move_strat, building):
         self.id = id
         self.move_strat = move_strat
         self.current_floor = 0
         self.riders = []
         self.state = "IDLE"
         self.direction = "UP"
+        self.building = building
 
-        self.tasks = []
-    
-    def get_tasks(self, tasks):
-        self.tasks = tasks
-    
-    def add_task(self, task):
-        self.tasks.append(task)
 
     def step(self, sim_time):
-        if not self.tasks:
-            self.state = "IDLE"
-            self.change_dir()
-            return
-        
-        self.tasks = self.move_strat(self.tasks, self.current_floor, self.direction)
-        task = self.tasks.pop(0)
-        trgt_floor = task["floor"]
-
-        self.move_to(trgt_floor)
-
-        if task["type"]=="move":
-            self.move_to(task["floor"])
-        elif task["type"]=="pickup":
-            self.pickup(task["person"], sim_time)
-        elif task["type"]=="dropoff":
-            self.dropoff(task["person"], sim_time)
-
-    def move_to(self, floor):
-        self.state = "MOVING"
-        print(f"ELEVATOR MOVING: Elevator {self.id} moving from floor {self.current_floor} to {floor}")
-        self.current_floor = floor
-        self.state = "ARRIVED"
-
-    def pickup(self, person, sim_time):
-        if len(self.riders) >= MAX_CAPACITY:
-            print(f"ELEVATOR {self.id} FULL: Cannot pick up person at floor {person.start_floor}")
-            return
-        person.pickup_time = sim_time
-        self.riders.append(person)
-        print(f"ELEVATOR {self.id} PICKUP: Person from {person.start_floor} to {person.dest_floor}")
-
-    def dropoff(self, person, sim_time):
-        if person in self.riders:
+        # 1. Drop off
+        exiting = [p for p in self.riders if p.dest_floor == self.current_floor]
+        for person in exiting:
             person.dropoff_time = sim_time
             self.riders.remove(person)
             print(f"ELEVATOR {self.id} DROPOFF: Person dropped off at floor {self.current_floor}")
 
-    def change_dir(self):
-        if self.direction=="UP" and self.current_floor >= FLOORS-1:
-            self.direction = "DOWN"
-        elif self.direction == "DOWN" and self.current_floor <= 0:
-            self.direction="UP"
+        # 2. Pick up
+        open_slots = MAX_CAPACITY - len(self.riders)
+        boarded = False
+        if open_slots > 0:
+            new_riders = self.building.get_boarding_passengers(
+                self.current_floor, self.direction, open_slots
+            )
+            for person in new_riders:
+                person.pickup_time = sim_time
+                self.riders.append(person)
+                print(f"ELEVATOR {self.id} PICKUP: Person from {person.start_floor} to {person.dest_floor}")
+                boarded = True
+
+        # 3. Decide next move
+        if not exiting and not boarded:
+            self.move()
+
+    def move(self):
+        if self.direction == "UP":
+            if self.current_floor < FLOORS - 1:
+                self.current_floor += 1
+            else:
+                self.direction = "DOWN"
+                self.current_floor -= 1
+        else:
+            if self.current_floor > 0:
+                self.current_floor -= 1
+            else:
+                self.direction = "UP"
+                self.current_floor += 1
+
+        print(f"ELEVATOR {self.id} moving {self.direction} to floor {self.current_floor}")
+
+
+
     
