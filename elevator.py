96e64dd9fe495ee02470b0f182d747df0243822a
nathanbrasse <nathanbrasseftw@gmail.com>
2025-04-10 05:49:28 -0400

    moved things around

--- elevator.py
+++ elevator.py
@@ -9,34 +9,33 @@ class Elevator:
     def __init__(self, id, move_strat, building):
         self.id = id
         self.move_strat = move_strat
+        self.building = building
+        
         self.current_floor = 0
-        self.riders = []
+
         self.state = "IDLE"
         self.direction = "UP"
-        self.building = building
+
+        self.riders = []
 
     def step(self, sim_time):
-        # 1. Drop off
+
         exiting = [p for p in self.riders if p.dest_floor == self.current_floor]
         for person in exiting:
             person.dropoff_time = sim_time
             self.riders.remove(person)
-            print(f"ELEVATOR {self.id} DROPOFF: Person dropped off at floor {self.current_floor}")
+            print(f"ELEVATOR {self.id} DROPOFF: Person {person.id} dropped off at floor {self.current_floor}")
 
-        # 2. Pick up
         open_slots = MAX_CAPACITY - len(self.riders)
         boarded = False
         if open_slots > 0:
-            new_riders = self.building.get_boarding_passengers(
-                self.current_floor, self.direction
-            )
+            new_riders = self.building.get_boarding_passengers(self.current_floor, self.direction)
             for person in new_riders:
                 person.pickup_time = sim_time
                 self.riders.append(person)
-                print(f"ELEVATOR {self.id} PICKUP: Person from {person.start_floor} to {person.dest_floor}")
+                print(f"ELEVATOR {self.id} PICKUP: Person {person.id} from {person.start_floor} to {person.dest_floor}")
                 boarded = True
 
-        # 3. Decide next move
         if not exiting and not boarded:
             self.move()
 
